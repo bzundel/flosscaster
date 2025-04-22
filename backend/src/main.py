5ba59b5e3b2c61cd4e3154ea146b9ab546bcdd17
Benedikt Zundel <benedikt.zundel@live.com>
2025-04-22 19:54:19 +0200

    backend: create endpoint with sqlite writing

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,44 @@
+import sqlite3
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from dataclasses import dataclass
+
+DATABASE_PATH = "../flosscaster.db"
 
 app = Flask(__name__)
 api = Api(app)
+
+@dataclass
+class Podcast:
+    title: str
+    description: str
+    date: str
 
 class Ping(Resource):
     def get(self):
         resp = "Pong!"
         return jsonify({"data": resp})
 
+class Create(Resource):
+    def post(self):
+        json = request.get_json()
+
+        title = json["title"]
+        description = json["description"]
+        date = json["date"]
+
+        con = sqlite3.connect(DATABASE_PATH)
+        cur = con.cursor()
+        cur.execute(f"INSERT INTO podcasts VALUES ('{title}', '{description}', '{date}')")
+        con.commit()
+
+        return "", 200
+
 api.add_resource(Ping, "/api/ping")
+api.add_resource(Create, "/api/create")
 
 if __name__ == "__main__":
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS podcasts(title, description, date)")
     app.run(debug = True)
