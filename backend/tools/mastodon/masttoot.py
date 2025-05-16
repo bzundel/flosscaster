6900c29fb8c17561b2765ee40d055d448e727f98
Dwippah <dwipa.flgel@yahoo.de>
2025-05-16 18:54:38 +0000

    added toot funtionality & a gitignore file for the .env file that contains the access information

--- backend/tools/mastodon/masttoot.py
+++ backend/tools/mastodon/masttoot.py
@@ -1,23 +1,42 @@
 import argparse
+import os
+import sys
+import time
+
 from mastodon import Mastodon
+from dotenv import load_dotenv
+import feedparser
 
-# Definiere den Access Token hier
-ACCESS_TOKEN = 'your_access_token_here'  # Ersetze dies durch deinen tatsächlichen Access Token
+def masttoot(title: str, url: str, description: str):
+    """Publishes a new toot using the input, also sends a reply to the announcement with the description."""
 
-def main():
-    # Argumente parsen
-    parser = argparse.ArgumentParser(description='Post a Toot on Mastodon.')
-    parser.add_argument('message', type=str, help='The message to post as a Toot')
+    #Load access information from .env file and create the initial announcement text
+    load_dotenv()
+    post_text = "The Flosscasters have released a new Podcast Episode: " + title + "Check it out @ " + url
 
-    args = parser.parse_args()
+    #Mastodon-Instanz
+    mastodon = Mastodon(
+        client_id=os.getenv("CLIENT_KEY"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        access_token=os.getenv("ACCESS_TOKEN"),
+        api_base_url="https://mastodon.social/"
+    )
 
-    # Mastodon-Instanz erstellen
-    mastodon = Mastodon(access_token=ACCESS_TOKEN)
+    #Create a list of current toots
+    toot_so_far = []
+    for posts in mastodon.account_statuses(os.getenv("ID")):
+        toot_so_far.append(posts.id)
 
-    # Toot erstellen
-    mastodon.toot(args.message)
-    print("Successfully posted Toot:", args.message)
+    #Publish the toot and waits 10s
+    mastodon.status_post(post_text)
+    time.sleep(10)
+
+    #Checks for a new toot on the account and replies with the description of the episode
+    for posts in mastodon.account_statuses(os.getenv("ID")):
+        if posts.id not in toot_so_far:
+            toot_so_far.append(posts.id)
+            mastodon.status_post(description,in_reply_to_id=posts.id)
 
 if __name__ == "__main__":
-    main()
+    masttoot(sys.argv[1], sys.argv[2], sys.argv[3])
 
