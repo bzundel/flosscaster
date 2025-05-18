41837762918a30da66af3248116071f81f386beb
saschaG2000 <saschagavrilov2000@outlook.de>
2025-05-18 21:36:34 +0200

    Change of name published to bar
    Change of name published to bar

---
+++ frontend/src/components/bar/bar.jsx
@@ -0,0 +1,8 @@
+import styles from "./bar.module.css";
+
+export const Bar = () => {
+  return (
+    <section className={styles.container}>
+    </section>
+  )
+}
--- frontend/src/components/published/published.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from "./published.module.css";
-
-export const Published = () => {
-  return (
-    <section className={styles.container}>
-    </section>
-  )
-}
