fb5c5dd0d156ac007e2e3228dcc5402af8348545
Ivan2344 <152505313+Ivan2344@users.noreply.github.com>
2025-05-19 23:02:20 +0200

    Update FeedEditorModule.py (edit order enclosure)

--- backend/tools/rsstests/Final/FeedEditorModule.py
+++ backend/tools/rsstests/Final/FeedEditorModule.py
@@ -29,8 +29,8 @@ def add_episode_to_podcast(title: str, url: str, description: str):
     etree.SubElement(new_item, 'pubDate').text = new_episode_pubDate
     enclosure = etree.SubElement(new_item, 'enclosure')
     enclosure.set('url', url)
-    enclosure.set('type', 'audio/mpeg')
     enclosure.set('length', str(os.path.getsize(os.path.join(os.environ['UPLOAD_PATH'], os.path.basename(url)))))
+    enclosure.set('type', 'audio/mpeg')
 
     # Füge das neue Item zum Channel hinzu
     channel = root.find('channel')
