3555b3246c4e78c8c27238eaf4582a715aac3ed1
dominikVibox <90802318+sgedodo@users.noreply.github.com>
2025-05-21 01:54:22 +0200

    TESTING implemented test for getrss

---
+++ backend/tests/test_getrss.py
@@ -0,0 +1,14 @@
+import os
+
+def test_getrss(create_rss, client):
+    response = client.get("/rss")
+    assert "application/xml" in response.headers["content-type"]
+    assert response.status_code == 200 or response.status_code == 304
+
+def test_getrss_no_file(delete_rss, client):
+    response = client.get("/rss")
+    assert response.status_code == 404
+    if not delete_rss == "failure":
+        rss_path = os.getenv("RSS_FILE")
+        with open(rss_path, "w") as file:
+            file.write(delete_rss)
