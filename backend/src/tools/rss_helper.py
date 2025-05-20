935b59240e4ad0ae63c3215cd5a47515e8776744
Ivan2344 <Ivan2344>
2025-05-20 09:45:33 +0200

    added enclosure length in code and .tex

--- backend/src/tools/rss_helper.py
+++ backend/src/tools/rss_helper.py
@@ -45,6 +45,7 @@ def add_episode_to_podcast(title: str, url: str, description: str):
     etree.SubElement(new_item, 'pubDate').text = new_episode_pub_date
     enclosure = etree.SubElement(new_item, 'enclosure')
     enclosure.set('url', url)
+    enclosure.set('length', str(os.path.getsize(os.path.join(os.environ['UPLOAD_PATH'], os.path.basename(url)))))
     enclosure.set('type', 'audio/mpeg')
 
     # Füge das neue Item zum Channel hinzu
