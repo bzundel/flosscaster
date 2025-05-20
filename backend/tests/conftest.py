5a6ee6e97f80a9e286df53e92ea6ef7640d3ebe4
dominikVibox <90802318+sgedodo@users.noreply.github.com>
2025-05-20 19:46:01 +0200

    TESTING Implemented test for Api List request

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,9 @@
+import uuid
+from datetime import datetime
+
 import pytest
 import os
 import sqlite3
-
-from gtts import gTTS
 
 from backend.src.main import app
 
@@ -12,8 +13,18 @@ DATA_PATH = os.getenv("FLOSSCASTER_DATA_PATH")
 @pytest.fixture()
 def init_testing():
     app.config.update({'TESTING': True})
+    if not os.path.exists(DATABASE_FILE):
+        con = sqlite3.connect(DATABASE_FILE)
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS podcasts(id INTEGER PRIMARY KEY, title, description, date, filepath)")
+        con.close()
     yield app
     app.config.update({'TESTING': False})
+
+@pytest.fixture()
+def client(init_testing):
+    with app.test_client() as client:
+        yield client
 
 @pytest.fixture()
 def empty_database(init_testing):
@@ -25,5 +36,15 @@ def empty_database(init_testing):
     con.close()
 
 @pytest.fixture()
-def client(init_testing):
-    return app.test_client()
+def database_entry(init_testing):
+    title = "Test entry"
+    description = "Test description"
+    date = datetime.now().strftime("%c")
+    filename = f"{str(uuid.uuid4())}.mp3"
+    con = sqlite3.connect(DATABASE_FILE)
+    cur = con.cursor()
+    cur.execute("INSERT INTO podcasts (id, title, description, date, filepath) VALUES (NULL, ?,?,?,?)", (title, description, date, filename))
+    lastid = cur.lastrowid
+    con.commit()
+    con.close()
+    return lastid
---
+++ backend/tests/test_list.py
@@ -0,0 +1,23 @@
+import os
+
+DATABASE_FILE = os.getenv("DATABASE_FILE")
+
+def test_list_empty_db(empty_database, client):
+    response = client.get("/api/list")
+    assert response.status_code == 200
+    assert b'[]' in response.data
+
+def test_list(database_entry, client):
+    response = client.get("/api/list")
+    assert response.status_code == 200
+    assert f"\"id\":{database_entry}".encode("utf8") in response.data
+    assert b"\"title\":" in response.data
+    assert b"\"date\":" in response.data
+    assert b"\"description\":" in response.data
+    assert b"\"filepath\":" in response.data
+
+def test_list_with_param(database_entry, client):
+    response = client.get("/api/list/")
+    response2 = client.get("/api/list/test")
+    assert response.status_code == 404
+    assert response2.status_code == 404
