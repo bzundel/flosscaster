adf9f4f5179ade4d70d99b44ca8379d6357d659e
dominikVibox <90802318+sgedodo@users.noreply.github.com>
2025-05-21 02:46:58 +0200

    TESTING added test for create api call, if it is not a audio file

--- backend/tests/test_create.py
+++ backend/tests/test_create.py
@@ -1,10 +1,7 @@
-import pytest
 import os
+import pytest
 
-from pathlib import Path
-
-from backend.src.main import UPLOAD_PATH
-from backend.tests.random_file_generator import generate_random_ascii, generate_random_ascii_path, generate_mp3_size, generate_mp3_tts, generate_mp3_duration
+from backend.tests.random_file_generator import generate_random_ascii, generate_mp3_size, generate_mp3_tts, generate_mp3_duration
 
 @pytest.mark.parametrize("title, description, audio", [
     ("First FLOSScast", "The very first flosscast", generate_mp3_tts("Hello to the very first flosscast episode.", lang="en")),
@@ -21,3 +18,16 @@ def test_create_podcasts(client, title, description, audio):
         "audio": open(audio, 'rb')
     })
     assert response.status_code == 200
+
+def test_create_not_audio(client):
+    test_file = os.path.join(os.getenv("FLOSSCASTER_DATA_PATH"), generate_random_ascii(length=24) + ".txt")
+    with open(test_file, "w") as f:
+        f.write(generate_random_ascii(length=1024, letters=True, numbers=True, punctuation=True))
+    response = client.post("/api/create", content_type="multipart/form-data",
+                           data={
+                               "title": generate_random_ascii(length=12, letters=True, numbers=False),
+                               "description": generate_random_ascii(length=24, letters=True, numbers=True),
+                               "audio": (open(test_file, 'rb'), "application/octet-stream")
+                           }
+                           )
+    assert response.status_code == 400
